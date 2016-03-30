7f5886eb0af54c462d5936a7a17747f31557368e
wan <wan@wan-PC>
2016-03-30 11:16:04 +0800

    挂件终于好了，耽误了好多时间，widget里面不能用view布局，这个是教训。

--- src/com/wanquanlong/weatherwidget/MainActivity.java
+++ src/com/wanquanlong/weatherwidget/MainActivity.java
@@ -1,43 +1,37 @@
 package com.wanquanlong.weatherwidget;
 
-import com.lidroid.xutils.exception.HttpException;
-import com.wanquanlong.weatherwidget.Bean.StatusBean;
-import com.wanquanlong.weatherwidget.api.HttpApi;
-import com.wanquanlong.weatherwidget.api.OnHttpRequestCallBack;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
+
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.LocationClientOption.LocationMode;
+import com.wanquanlong.weatherwidget.Bean.StatusBean;
 
 public class MainActivity extends Activity {
 
 	TextView tView;
 	StatusBean statusBean;
+	String cityname = null;
+
+	public LocationClient mLocationClient = null;
+	public BDLocationListener myListener = new MyLocationListener();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		tView = (TextView) findViewById(R.id.tv_ss);
-		HttpApi.getJsonData("…Ó€⁄", new OnHttpRequestCallBack<StatusBean>() {
+		// initData("…Ó€⁄");
 
-			@Override
-			public void onSuccess(StatusBean result) {
-				statusBean = result;
-				Log.e("statusBean", statusBean.toString());
-				if (statusBean != null) {
-					String[] yundong = statusBean.getResult().getData()
-							.getLife().getInfo().getYundong();
-					tView.setText(yundong[1]);
-				}
+		
 
-			}
-
-			@Override
-			public void onFailure(HttpException exception, String errorString) {
-
-			}
-		});
 	}
+
+	
+
+	
+
 }
